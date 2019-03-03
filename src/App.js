3fb0317a198d8ad9374cb29b2cb4ff34e1409d8b
crong <nigayoun@gmail.com>
2019-03-03 09:36:49 +0900

    hooks1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import List from './List.jsx';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [todos, setTodos] = useState(['js공부']);
+  const [newTodo, setNewTodo] = useState();
+
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
   }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  }
+
+  useEffect( ()=> {
+    console.log("새로운 내용이 렌더링됐네요", todos);
+  }, [todos])
+
+  return (
+    <>
+    <h1>todo 애플리케이션</h1>
+
+    <form action="">
+      <input type="text" name="" onChange={changeInputData}/>
+      <button onClick={addTodo}>할일추가</button>
+    </form>
+
+    <List todos={todos} />
+    </>
+  )
 }
 
 export default App;
